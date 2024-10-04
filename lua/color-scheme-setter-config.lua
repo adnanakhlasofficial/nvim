3e3cd278a81f9b73e598ee46476a86cc3f7d707e
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-10-04 13:06:39 +0600

    nvim update after win11 24h2

--- lua/color-scheme-setter-config.lua
+++ lua/color-scheme-setter-config.lua
@@ -11,16 +11,16 @@
 
 
 -- === For OneDark theme
-require('onedark').setup {
-	style = 'deep'
-}
+-- require('onedark').setup {
+-- 	style = 'deep'
+-- }
 
-require('onedark').load()
+-- require('onedark').load()
 -- === END
 
 -- === For TokyoNight theme
--- require('tokyonight').setup {
--- 	style = 'moon'
--- }
--- require('tokyonight').load()
+require('tokyonight').setup {
+	style = 'moon'
+}
+require('tokyonight').load()
 -- === END
